2735568fecee70fa44091414bb987d9de51f6e70
AntonioAlvarado21 <jcruza1300@alumno.ipn.mx>
2022-10-19 15:24:06 -0500

    Uso de SimpleDialog

--- lib/src/screens/my_home_page.dart
+++ lib/src/screens/my_home_page.dart
@@ -10,50 +10,40 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        key: _scaffKey,
-        appBar: AppBar(title: const Text("Mostrar un snackbar")),
-        body: const ShowSnackBar());
-  }
-}
-
-class ShowSnackBar extends StatelessWidget {
-  const ShowSnackBar({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
+      key: _scaffKey,
+      appBar: AppBar(title: const Text("Mostrar un snackbar")),
+      body: Center(
         child: TextButton(
+            child: const Text("Mostrar"),
             onPressed: () {
-              final snackbar = SnackBar(
-                  action: SnackBarAction(
-                    label: "Cancelar",
-                    textColor: Colors.blue.shade50,
-                    onPressed: () {
-                      print("Se cancelo la acción");
-                    },
-                  ),
-                  backgroundColor: Colors.redAccent,
-                  duration: const Duration(seconds: 3),
-                  content: Row(
-                    children: const [
-                      Icon(
-                        Icons.thumb_up,
-                        color: Colors.white,
-                      ),
-                      SizedBox(
-                        width: 16,
-                      ),
-                      Text(
-                        "Deleted action sucessful",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w400,
-                            fontSize: 18),
-                      )
-                    ],
-                  ));
-              ScaffoldMessenger.of(context).showSnackBar(snackbar);
-            },
-            child: const Text("Mostrar Snackbar")));
+              _showDialog(context);
+            }),
+      ),
+    );
+  }
+
+  void _showDialog(BuildContext context) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return SimpleDialog(title: const Text("Seleccione"), children: <Widget>[
+            ListTile(
+                title: const Text("Eliminar"),
+                leading: const Icon(Icons.delete),
+                onTap: () {
+                  Navigator.pop(context);
+                }),
+            ListTile(
+              title: const Text("Editar"),
+              leading: const Icon(Icons.edit),
+              onTap: () {
+                Navigator.pop(context);
+              },
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10) ),
+            )
+          ]);
+        },
+        //barrierDismissible: false 
+      );
   }
 }
