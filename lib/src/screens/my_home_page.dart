e4e1f014ee51279c3605c7744effafa72744e2b3
AntonioAlvarado21 <jcruza1300@alumno.ipn.mx>
2022-10-19 18:38:18 -0500

    Uso plugin image_picker

--- lib/src/screens/my_home_page.dart
+++ lib/src/screens/my_home_page.dart
@@ -1,20 +1,88 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class MyHomePage extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File? _image;
+
+  Future getImage(ImageSource source) async {
+    try {
+      final image = await ImagePicker().pickImage(source: source);
+      if (image == null) return;
+
+      final imageTemporaly = File(image.path);
+
+      setState(() {
+        this._image = imageTemporaly;
+      });
+    } on PlatformException catch (e) {
+      print("Failed to pick image: $e");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("Fade In Image")),
+        appBar: AppBar(title: const Text("Pick an Image")),
         body: Center(
-            child: FadeInImage.assetNetwork(
-          image:
-              "https://i.pinimg.com/originals/0a/4d/cb/0a4dcb92fa2d3c601b58d72720d6bec4.jpg",
-          placeholder: "assets/loading.gif",
-          height: 600,
-          fit: BoxFit.fitHeight,
-          fadeInCurve: Curves.easeOutCirc,
+            child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            _image != null
+                ? Image.file(
+                    _image!,
+                    width: 300,
+                    height: 300,
+                    fit: BoxFit.fill,
+                  )
+                : const Text("Selecciona una imagen"),
+            const SizedBox(
+              height: 20,
+            ),
+            CustomButton(
+                title: "Pick from Gallery",
+                icon: Icons.image_outlined,
+                onClick: () => getImage(ImageSource.gallery)),
+            const SizedBox(
+              height: 10,
+            ),
+            CustomButton(
+                title: "Take a photo",
+                icon: Icons.camera_alt,
+                onClick: () => getImage(ImageSource.camera)),
+          ],
         )));
   }
+}
+
+Widget CustomButton(
+    {required String title,
+    required IconData icon,
+    required VoidCallback onClick}) {
+  return SizedBox(
+    width: 200,
+    child: ElevatedButton(
+      onPressed: onClick,
+      child: Row(
+        children: [
+          Icon(icon),
+          const SizedBox(
+            width: 20,
+          ),
+          Text(title)
+        ],
+      ),
+    ),
+  );
 }
